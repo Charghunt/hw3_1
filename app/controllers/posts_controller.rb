92c259688cd08e18d0f446d17219a6cb50605019
Charlotte Gunn Hunt <charlotte.gunn@kellogg.northwestern.edu>
2023-05-17 17:33:50 +0000

    Have posts showing

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,9 +15,10 @@ class PostsController < ApplicationController
     @post["title"] = params["post"]["title"]
     @post["description"] = params["post"]["description"]
     @post["posted_on"] = params["post"]["posted_on"]
-    @post["post_id"] = params["post"]["post_id"]
-  
+    @post["place_id"] = params["post"]["place_id"]
+
     @post.save
+    
     redirect_to "/places/#{@post["place_id"]}"
   end
   

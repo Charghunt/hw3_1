aeb454150a7c30f2682da58bd73566710e23600e
Charlotte Gunn Hunt <charlotte.gunn@kellogg.northwestern.edu>
2023-05-17 16:48:47 +0000

    fixed posts logic

--- app/controllers/post_controller.rb
+++
@@ -1,9 +0,0 @@
-def create
-  @post = Post.new
-  @post["title"] = params["post"]["title"]
-  @post["post_id"] = params["post"]["post_id"]
-  @post.save
-  redirect_to "/places/#{@post["place_id"]}"
-end
-
-end
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,8 @@
 
 Rails.application.routes.draw do
   resources "places"
-  resources "post"
+  resources "posts"
 end
+
 
   
